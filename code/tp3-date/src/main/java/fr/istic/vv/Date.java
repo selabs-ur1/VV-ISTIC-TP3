97cce10e04f53771412fe60ff90d9f1cc7383db8
Danlos <benjamin.danlos@etudiant.univ-rennes1.fr>
2022-03-12 12:55:37 +0100

    impl + tests Date

--- code/tp3-date/src/main/java/fr/istic/vv/Date.java
+++ code/tp3-date/src/main/java/fr/istic/vv/Date.java
@@ -1,17 +1,122 @@
 package fr.istic.vv;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 class Date implements Comparable<Date> {
 
-    public Date(int day, int month, int year) { }
+    private int day;
+    private int month;
+    private int year;
+    private static final List<Integer> longMonths = Arrays.asList(1,3,5,7,8,10,12);
 
-    public static boolean isValidDate(int day, int month, int year) { return false; }
+    public int getDay() {
+        return day;
+    }
 
-    public static boolean isLeapYear(int year) { return false; }
+    public int getMonth() {
+        return month;
+    }
 
-    public Date nextDate() { return null; }
+    public int getYear() {
+        return year;
+    }
 
-    public Date previousDate() { return null; }
+    public Date(int day, int month, int year) {
+        if(isValidDate(day, month, year)){
+            this.day=day;
+            this.month=month;
+            this.year=year;
+        }
+        else{
+            throw new IllegalArgumentException("Invalid date");
+        }
+    }
 
-    public int compareTo(Date other) { return 0; }
+    public static boolean isValidDate(int day, int month, int year) {
+        if(day<1 || day>31){
+            return false;
+        }
+        if(month<1 || month>12){
+            return false;
+        }
+        if(day==29 && month==2){
+            return isLeapYear(year);
+        }
+        if(!longMonths.contains(month) && day==31){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isLeapYear(int year) {
+        return year%4==0 && year%100!=0;
+    }
+
+    public Date nextDate() {
+        //general case and leap year : only day increases
+        if(day<28 || (day==28 && isLeapYear(year))){
+            return new Date(day+1, month, year);
+        }
+        //if day is 30 and month can go up to 31, increase day
+        if(day==30 && longMonths.contains(month)){
+            return new Date(day+1, month, year);
+        }
+        //only possibility for day now is to be 31
+        //if month is december, increase year and 1st january
+        if(month==12){
+            return new Date(1, 1, year+1);
+        }
+        //otherwise increase month and day is 1st
+        return new Date(1, month+1, year);
+    }
+
+    public Date previousDate() {
+        if(day==1){
+            if(month==1){
+                return new Date(31,12,year-1);
+            }
+            else{
+                //if previous month is a 31 day month
+                if(longMonths.contains(month-1)){
+                    return new Date(31, month-1, year);
+                }
+                else{
+                    return new Date(30, month-1, year);
+                }
+            }
+        }
+        else{
+            return new Date(day-1, month, year);
+        }
+    }
+
+    public int compareTo(Date other) {
+        Objects.requireNonNull(other);
+        /*if(day==other.getDay() && month==other.getMonth() && year==other.getYear()){
+            return 0;
+        }*/
+        if(year!=other.year){
+            return year-other.getYear();
+        }
+        else{
+            if(month!=other.getMonth()){
+                return month-other.getMonth();
+            }
+            else{
+                return day-other.getDay();//if == returns 0
+            }
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Date date = (Date) o;
+        return getDay() == date.getDay() && getMonth() == date.getMonth() && getYear() == date.getYear();
+    }
 
 }
--- code/tp3-date/src/test/java/fr/istic/vv/DateTest.java
+++ code/tp3-date/src/test/java/fr/istic/vv/DateTest.java
@@ -2,9 +2,255 @@ package fr.istic.vv;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DateTest {
 
+    @Test
+    void testConstructorThrow(){
+        assertThrows(IllegalArgumentException.class, ()->new Date(0, 0, 0));
+    }
 
+    @Test
+    void isValidDateNominal1() {
+        assertTrue(Date.isValidDate(1,1,1970));
+    }
+
+    @Test
+    void isValidDateNominal2() {
+        assertTrue(Date.isValidDate(31,12,1970));
+    }
+
+    @Test
+    void isValidDateBissextile2902() {
+        assertTrue(Date.isValidDate(29,2,2020));
+    }
+
+    @Test
+    void isValidDateNotBissextile2902() {
+        assertFalse(Date.isValidDate(29,2,2022));
+    }
+
+    @Test
+    void isValidDateNominal3() {
+        assertTrue(Date.isValidDate(28,2,2022));
+    }
+
+    @Test
+    void isValidDateNominal4() {
+        assertTrue(Date.isValidDate(30,6,2022));
+    }
+
+    @Test
+    void isValidDate3106() {
+        assertFalse(Date.isValidDate(31,6,2022));
+    }
+
+    @Test
+    void isValidDateNominal5() {
+        assertTrue(Date.isValidDate(31,7,2022));
+    }
+
+    @Test
+    void isValidDateday32() {
+        assertFalse(Date.isValidDate(32,7,2022));
+    }
+
+    @Test
+    void isValidDateNominal6() {
+        assertTrue(Date.isValidDate(3,9,2022));
+    }
+
+    @Test
+    void isValidDatemonth13() {
+        assertFalse(Date.isValidDate(3,13,2022));
+    }
+
+    @Test
+    void isValidDateDay0() {
+        assertFalse(Date.isValidDate(0,1,2022));
+    }
+
+    @Test
+    void isValidDateMonth0() {
+        assertFalse(Date.isValidDate(3,0,2022));
+    }
+
+    @Test
+    void isValidDateYearNegative() {
+        assertFalse(Date.isValidDate(3,13,-2022));
+    }
+
+    @Test
+    void isValidDateDayNegative() {
+        assertFalse(Date.isValidDate(-3,1,2022));
+    }
+
+    @Test
+    void isValidDateMonthNegative() {
+        assertFalse(Date.isValidDate(3,-1,2022));
+    }
+
+    @Test
+    void isLeapYear2020() {
+        assertTrue(Date.isLeapYear(2020));
+    }
+
+    @Test
+    void isLeapYear2022() {
+        assertFalse(Date.isLeapYear(2022));
+    }
+
+    @Test
+    void isLeapYear2000() {
+        assertFalse(Date.isLeapYear(2000));
+    }
+
+    @Test
+    void isLeapYear2100() {
+        assertFalse(Date.isLeapYear(2100));
+    }
+
+    @Test
+    void nextDateSameMonth31days() {
+        Date currentDate = new Date(1,1,2022);
+        Date expected = new Date(2,1,2022);
+        assertEquals(expected, currentDate.nextDate());
+    }
+    @Test
+    void nextDateSameMonth31daysLastDay() {
+        Date currentDate = new Date(30,1,2022);
+        Date expected = new Date(31,1,2022);
+        assertEquals(expected, currentDate.nextDate());
+    }
+
+    @Test
+    void nextDateSameMonth30days() {
+        Date currentDate = new Date(1,4,2022);
+        Date expected = new Date(2,4,2022);
+        assertEquals(expected, currentDate.nextDate());
+    }
+
+    @Test
+    void nextDateNominalNextMonth31days() {
+        Date currentDate = new Date(31,1,2022);
+        Date expected = new Date(1,2,2022);
+        assertEquals(expected, currentDate.nextDate());
+    }
+
+    @Test
+    void nextDateNominalNextMonth30days() {
+        Date currentDate = new Date(30,4,2022);
+        Date expected = new Date(1,5,2022);
+        assertEquals(expected, currentDate.nextDate());
+    }
+
+    @Test
+    void nextDateNominalNextYear() {
+        Date currentDate = new Date(31,12,2022);
+        Date expected = new Date(1,1,2023);
+        assertEquals(expected, currentDate.nextDate());
+    }
+
+    @Test
+    void nextDateNominalNextMonthBissextile() {
+        Date currentDate = new Date(28,2,2020);
+        Date expected = new Date(29,2,2020);
+        assertEquals(expected, currentDate.nextDate());
+    }
+
+    @Test
+    void nextDateNominalNextMonthNotBissextile() {
+        Date currentDate = new Date(28,2,2022);
+        Date expected = new Date(1,3,2022);
+        assertEquals(expected, currentDate.nextDate());
+    }
+
+    @Test
+    void previousDateNominal() {
+        Date currentDate = new Date(31,7,2022);
+        Date expected = new Date(30,7,2022);
+        assertEquals(expected, currentDate.previousDate());
+    }
+
+    @Test
+    void previousDatePreviousMonth30days() {
+        Date currentDate = new Date(1,7,2022);
+        Date expected = new Date(30, 6, 2022);
+        assertEquals(expected, currentDate.previousDate());
+    }
+
+    @Test
+    void previousDatePreviousMonthAlso31days() {
+        Date currentDate = new Date(1,8,2022);
+        Date expected = new Date(31,7,2022);
+        assertEquals(expected, currentDate.previousDate());
+    }
+
+    @Test
+    void previousDatePreviousMonth31days() {
+        Date currentDate = new Date(1,9, 2022);
+        Date expected = new Date(31, 8, 2022);
+        assertEquals(expected, currentDate.previousDate());
+    }
+
+    @Test
+    void previousDatePreviousYear() {
+        Date currentDate = new Date(1,1,2022);
+        Date expected = new Date(31,12,2021);
+        assertEquals(expected, currentDate.previousDate());
+    }
+
+    @Test
+    void compareToBeforeDay() {
+        Date d1 = new Date(3,9,2022);
+        Date d2 = new Date(4,9,2022);
+        assertTrue(d1.compareTo(d2) < 0);
+    }
+    
+    @Test
+    void compareToBeforeMonth() {
+        Date d1 = new Date(3,9,2022);
+        Date d2 = new Date(3,10,2022);
+        assertTrue(d1.compareTo(d2) < 0);
+    }
+    
+    @Test
+    void compareToBeforeYear() {
+        Date d1 = new Date(3,9,2022);
+        Date d2 = new Date(3,9,2023);
+        assertTrue(d1.compareTo(d2) < 0);
+    }
+    
+    
+    @Test
+    void compareToEqual() {
+        Date d1 = new Date(3,9,2022);
+        Date d2 = new Date(3,9,2022);
+        assertEquals(0, d1.compareTo(d2));
+    }
+
+    @Test
+    void compareToAfterDay() {
+        Date d1 = new Date(4,9,2022);
+        Date d2 = new Date(3,9,2022);
+        assertTrue(d1.compareTo(d2) > 0);
+    }
+
+    @Test
+    void compareToAfterMonth() {
+        Date d1 = new Date(3,10,2022);
+        Date d2 = new Date(3,9,2022);
+        assertTrue(d1.compareTo(d2) > 0);
+    }
+
+    @Test
+    void compareToAfterYear() {
+        Date d1 = new Date(3,9,2023);
+        Date d2 = new Date(3,9,2022);
+        assertTrue(d1.compareTo(d2) > 0);
+    }
 }
