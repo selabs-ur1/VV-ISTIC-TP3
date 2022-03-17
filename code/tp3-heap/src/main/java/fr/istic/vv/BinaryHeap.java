efaa35bb5a4dc736b4297e1a9da9014d06cd1e48
Danlos <benjamin.danlos@etudiant.univ-rennes1.fr>
2022-03-17 14:48:26 +0100

    premiere implementation heap

--- code/tp3-heap/src/main/java/fr/istic/vv/BinaryHeap.java
+++ code/tp3-heap/src/main/java/fr/istic/vv/BinaryHeap.java
@@ -1,17 +1,75 @@
 package fr.istic.vv;
 
+
 import java.util.Comparator;
+import java.util.Objects;
 
 class BinaryHeap<T> {
 
-    public BinaryHeap(Comparator<T> comparator) {  }
+    private T value;
+    private Comparator<T> comparator;
+    private BinaryHeap<T> left;
+    private BinaryHeap<T> right;
 
-    public T pop() { return null; }
+    public BinaryHeap<T> getLeft() {
+        return left;
+    }
 
-    public T peek() { return null; }
+    public BinaryHeap<T> getRight() {
+        return right;
+    }
 
-    public void push(T element) {  }
+    public BinaryHeap(Comparator<T> comparator) {
+        this.comparator=comparator;
+    }
 
-    public int count() { return 0; }
+    public T pop() {
+        T v = value;
+        if(left!=null){
+            value = left.pop();
+        }
+        else{
+            if(right!=null){
+                value = right.pop();
+            }
+            else{
+                value = null;//delete this
+            }
+        }
+        return v;
+    }
+
+    public T peek() {
+        return value;
+    }
+
+    public void push(T element) {
+        if(comparator.compare(value, element)<=0){
+            if(left==null){
+                value = element;
+            }
+            else {
+                left.push(element);
+            }
+        }
+        else{
+            if(right==null){
+                value = element;
+            }else{
+                right.push(element);
+            }
+        }
+    }
+
+    public int count() {
+        int v = (value==null)?0:1;
+        if(left!=null){
+            v+=left.count();
+        }
+        if(right!=null){
+            v+= right.count();
+        }
+        return v;
+    }
 
 }
