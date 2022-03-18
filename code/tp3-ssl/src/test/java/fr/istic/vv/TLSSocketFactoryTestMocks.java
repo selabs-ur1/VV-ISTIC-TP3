03e86b7b258ed4d1b14bea389e3f62a211d0357e
Danlos <benjamin.danlos@etudiant.univ-rennes1.fr>
2022-03-18 16:27:09 +0100

    q6 mocks

--- code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
+++ code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
@@ -1,15 +1,70 @@
 package fr.istic.vv;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class TLSSocketFactoryTestMocks {
 
+    @Mock
+    private SSLSocket mockSSLSocket;
 
+    @BeforeEach
+    public void beforeEach(){
+        mockSSLSocket = mock(SSLSocket.class);
+    }
+
+    @Test
+    public void preparedSocket_NullProtocols() {
+        TLSSocketFactory f = new TLSSocketFactory();
+
+        when(mockSSLSocket.getEnabledProtocols())
+                .thenReturn(null);
+
+        when(mockSSLSocket.getSupportedProtocols())
+                .thenReturn(null);
+
+        doNothing().when(mockSSLSocket).setEnabledProtocols(any());
+
+        f.prepareSocket(mockSSLSocket);
+
+        verify(mockSSLSocket, times(1)).getEnabledProtocols();
+        verify(mockSSLSocket, times(1)).getSupportedProtocols();
+        //setEnabledProtocols should not be called when its argument is null or empty
+        verify(mockSSLSocket, times(0)).setEnabledProtocols(new String[]{});
+    }
+
+    @Test
+    public void typical()  {
+        TLSSocketFactory f = new TLSSocketFactory();
+
+        when(mockSSLSocket.getSupportedProtocols())
+                .thenReturn(shuffle(new String[]{"SSLv2Hello", "SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"}));
+        when(mockSSLSocket.getEnabledProtocols())
+                .thenReturn(shuffle(new String[]{"SSLv3", "TLSv1"}));
+        doNothing().when(mockSSLSocket).setEnabledProtocols(any());
+
+        f.prepareSocket(mockSSLSocket);
+
+        verify(mockSSLSocket, times(1)).getEnabledProtocols();
+        verify(mockSSLSocket, times(1)).getSupportedProtocols();
+        //verify that the arguments passed to setEnabledProtocols are correct
+        verify(mockSSLSocket, times(1)).setEnabledProtocols(new String[]{"TLSv1.2", "TLSv1.1",
+                "TLSv1", "SSLv3"});
+    }
+
+
+    private String[] shuffle(String[] in) {
+        List<String> list = new ArrayList<String>(Arrays.asList(in));
+        Collections.shuffle(list);
+        return list.toArray(new String[0]);
+    }
 }
