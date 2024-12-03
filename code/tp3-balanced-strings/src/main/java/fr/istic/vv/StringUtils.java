9f973c5142a5b8d7c0eedc35962551f2299566a9
jletouze <julien.letouze@etudiant.univ-rennes1.fr>
2024-12-03 09:17:10 +0100

    Exercice 3 méthode implémentée

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -1,11 +1,35 @@
 package fr.istic.vv;
 
+import java.util.Stack;
+
 public class StringUtils {
 
-    private StringUtils() {}
+    private StringUtils() {
+    }
 
     public static boolean isBalanced(String str) {
-        return false;
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            if (c == '{' || c == '[' || c == '(') {
+                stack.push(c);
+            } else if (c == '}' || c == ']' || c == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (c == '}' && top != '{' ||
+                        c == ']' && top != '[' ||
+                        c == ')' && top != '(') {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
     }
 
 }
