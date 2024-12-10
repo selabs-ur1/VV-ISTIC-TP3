ffeb6d82178d7021f4e2423f69954dbc8b8c64d1
Aurélien Durier <aurelien.durier@etudiant.univ-rennes.fr>
2024-12-10 15:28:14 +0100

    Exercice 2 et 3

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -1,11 +1,37 @@
 package fr.istic.vv;
 
+import java.util.Stack;
+
 public class StringUtils {
 
     private StringUtils() {}
 
     public static boolean isBalanced(String str) {
-        return false;
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            switch (c) {
+                case '(':
+                case '[':
+                case '{':
+                    stack.push(c);
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                    break;
+            }
+        }
+        return stack.isEmpty();
     }
-
 }
--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -1,11 +1,97 @@
 package fr.istic.vv;
 
 import org.junit.jupiter.api.Test;
-
-import static fr.istic.vv.StringUtils.isBalanced;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
 
+    @Test
+    void testEmptyString() {
+        assertTrue(StringUtils.isBalanced(""));
+    }
 
+    @Test
+    void testSingleTypeBalanced() {
+        assertTrue(StringUtils.isBalanced("()"));
+    }
+
+    @Test
+    void testMixedTypesProperlyNested() {
+        assertTrue(StringUtils.isBalanced("([])"));
+    }
+
+    @Test
+    void testSingleTypeImproperlyOrdered() {
+        assertFalse(StringUtils.isBalanced(")("));
+    }
+
+    @Test
+    void testMixedTypesImproperlyNestedRound() {
+        assertFalse(StringUtils.isBalanced("([)]"));
+    }
+
+    @Test
+    void testMixedTypesImproperlyNestedSquare() {
+        assertFalse(StringUtils.isBalanced("[(])"));
+    }
+
+    @Test
+    void testMixedTypesImproperlyNestedCurly() {
+        assertFalse(StringUtils.isBalanced("{(})"));
+    }
+
+    @Test
+    void testSingleTypeBalancedWithCurlyBraces() {
+        assertTrue(StringUtils.isBalanced("{{}}"));
+    }
+
+    @Test
+    void testMixedTypesProperlyNestedAllTypes() {
+        assertTrue(StringUtils.isBalanced("{[()]}"));
+    }
+
+    @Test
+    void testUnbalancedExtraOpeningBracket() {
+        assertFalse(StringUtils.isBalanced("((()"));
+    }
+
+    @Test
+    void testUnbalancedExtraClosingBracket() {
+        assertFalse(StringUtils.isBalanced("[{}]]"));
+    }
+
+    @Test
+    void testNonBracketCharacters() {
+        assertTrue(StringUtils.isBalanced("a"));
+    }
+
+    @Test
+    void testSingleUnmatchedOpeningRoundBracket() {
+        assertFalse(StringUtils.isBalanced("("));
+    }
+
+    @Test
+    void testSingleUnmatchedOpeningSquareBracket() {
+        assertFalse(StringUtils.isBalanced("["));
+    }
+
+    @Test
+    void testSingleUnmatchedOpeningCurlyBracket() {
+        assertFalse(StringUtils.isBalanced("{"));
+    }
+
+    @Test
+    void testSingleUnmatchedClosingBracket() {
+        assertFalse(StringUtils.isBalanced("]"));
+    }
+
+    @Test
+    void testBaseChoiceRoundImbalance() {
+        assertFalse(StringUtils.isBalanced(")"));
+    }
+
+    @Test
+    void testBaseChoiceCurlyImbalance() {
+        assertFalse(StringUtils.isBalanced("}"));
+    }
 }
