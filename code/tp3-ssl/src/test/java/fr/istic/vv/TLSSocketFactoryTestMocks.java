a12be0e99c8e93862415aa43424c82add768bf0c
alexxbout <boutinaud.pro@gmail.com>
2024-12-13 08:35:09 +0100

    added mocked tests

--- code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
+++ code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
@@ -7,9 +7,41 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
-public class TLSSocketFactoryTestMocks {
+class TLSSocketFactoryTestMocks {
 
+    @Test
+    void preparedSocket_NullProtocols() {
+        TLSSocketFactory f = new TLSSocketFactory();
+        SSLSocket socket = mock(SSLSocket.class);
+
+        when(socket.getSupportedProtocols()).thenReturn(null);
+        when(socket.getEnabledProtocols()).thenReturn(null);
+
+        f.prepareSocket(socket);
+
+        verify(socket, never()).setEnabledProtocols(any());
+    }
+
+    @Test
+    void typical() {
+        TLSSocketFactory f = new TLSSocketFactory();
+        SSLSocket socket = mock(SSLSocket.class);
+
+        when(socket.getSupportedProtocols()).thenReturn(shuffle(new String[]{"SSLv2Hello", "SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"}));
+        when(socket.getEnabledProtocols()).thenReturn(shuffle(new String[]{"SSLv3", "TLSv1"}));
+
+        f.prepareSocket(socket);
+
+        verify(socket).setEnabledProtocols(new String[]{"TLSv1.2", "TLSv1.1", "TLSv1", "SSLv3"});
+    }
+
+    private String[] shuffle(String[] in) {
+        List<String> list = new ArrayList<>(Arrays.asList(in));
+        Collections.shuffle(list);
+        return list.toArray(new String[0]);
+    }
 
 }
