bab85130b64028f60ab61a29be36e453c2d7e631
adaumand <anaelle.daumand@etudiant.univ-rennes.fr>
2024-12-17 22:44:38 +0100

    Exercice 4 terminé, dont le code se trouve dans le dossier code et le rapport dans le .md associé dans le dossier exercises. Les rapports de PIT sont aussi dans le dossier code.

--- code/tp3-date/src/main/java/fr/istic/vv/Date.java
+++ code/tp3-date/src/main/java/fr/istic/vv/Date.java
@@ -2,16 +2,155 @@ package fr.istic.vv;
 
 class Date implements Comparable<Date> {
 
-    public Date(int day, int month, int year) { }
+    int day;
+    int month;
+    int year;
 
-    public static boolean isValidDate(int day, int month, int year) { return false; }
+    public Date(int day, int month, int year) {
+        if(!isValidDate(day, month, year)) throw new IllegalArgumentException("Date invalide");
+        this.day=day;
+        this.month=month;
+        this.year=year;
+    }
 
-    public static boolean isLeapYear(int year) { return false; }
+    public static boolean isValidDate(int day, int month, int year) {
+        //cas de l'annee
+        if(year<=0) return false;
+        //cas des mois
+        if(month<1 || month >12) return false;
+        //cas des jours inférieurs à 1 ou supérieur à 31
+        if(day<1 || day>31) return false;
 
-    public Date nextDate() { return null; }
+        //cas du mois de Février qui change selon si l'annee est bissextile ou non
+        if(month == 2){
+            if(isLeapYear(year)) return day<=29;
+            else return day<=28;
+        }
 
-    public Date previousDate() { return null; }
+        //cas général, pour les mois à 30 jours
+        if(month == 4 || month == 6 || month == 9 || month == 11) return day <=30;
+        //sinon, il s'agit de mois à 31 jours
+        return day <= 31;
+    }
 
-    public int compareTo(Date other) { return 0; }
+    public static boolean isLeapYear(int year) {
+        return year % 400 == 0 || (year % 4 == 0 && year %100 !=0);
+    }
+
+    public Date nextDate() {
+        int day = this.day;
+        int month = this.month;
+        int year = this.year;
+
+        //cas de la fin d'annee
+        if(day == 31 && month == 12){
+            return new Date(1,1,year+1);
+        }
+
+        //cas de la fin de mois de 30 jours
+        else if(day == 30 && (month == 4 || month == 6 || month == 9 || month == 11)){
+            return new Date(1, month+1, year);
+        }
+
+        //cas de la fin de mois de 31 jours
+        else if(day == 31 && (month == 1 || month == 3 || month == 5 
+                || month == 7 || month == 8 || month == 10)){
+            return new Date(1, month+1, year);
+        }
+
+        //cas du mois de Février en annee non bissextile
+        else if(day == 28 && month == 2 && !isLeapYear(year)){
+            return new Date(1, month+1, year);
+        }
+
+        //cas du mois de Février durant une annee bissextile
+        else if(day == 29 && month == 2 && isLeapYear(year)){
+            return new Date(1, month+1, year);
+        }
+
+        //cas des autres jours des mois
+        else return new Date(day+1, month, year);
+    }
+
+    public Date previousDate() { 
+        int day = this.day;    
+        int month = this.month;
+        int year = this.year;
+
+        //cas du premier d'une annee (donc nouvel an)
+        if(day == 1 && month == 1) return new Date(31, 12, year-1);
+
+        //cas du premier des mois dont le mois précédent possède 30 jours
+        if(day == 1 && (month == 5 || month == 7  || month == 10 || month == 12)){
+            return new Date(30, month-1, year);
+        }
+
+        //cas du premier des mois dont le mois précédent contient 31 jours
+        else if (day == 1 && (month == 2 || month == 4 || month == 6 || month == 8 
+                                || month == 9 || month == 11)){
+            return new Date(31, month-1, year);
+        }
+
+        //cas du premier du mois de Mars en annee non bissextile
+        else if(day == 1 && month == 3 && !isLeapYear(year)) {
+            return new Date(28, month-1, year);
+        }
+
+        //cas du premier du mois de Mars en annee bissextile
+        else if(day == 1 && month == 3 && isLeapYear(year)) {
+            return new Date(29, month-1, year);
+        }
+
+        //tous les autres jours des mois
+        else{
+            return new Date(day-1, month, year);
+        }
+    }
+
+    /**
+     * compareTo follows the Comparable convention:
+
+     date.compareTo(other) returns a positive integer if date is posterior to other
+     date.compareTo(other) returns a negative integer if date is anterior to other
+     date.compareTo(other) returns 0 if date and other represent the same date.
+     the method throws a NullPointerException if other is null
+
+     */
+    public int compareTo(Date other) {
+        //cas de la date "other" étant nulle
+        if(other == null) throw new NullPointerException("Impossible de comparer avec une date nulle");
+        
+        //cas de l'annee différente donc soit inferieure soit superieure
+        if(this.year != other.year){
+            //si date est inferieure a other, alors -1 sinon +1
+           return (this.year < other.year)?-1:1;
+        }
+
+        //si les annees sont les memes, alors on passe aux mois
+        if(this.month != other.month){
+            /*si les mois sont différents, que le mois de date est inferieur à celui de other,
+             alors on retourne -1 sinon +1
+            */
+            return (this.month < other.month)?-1:1;
+        }
+
+        //si les mois sont également identiques, alors faire de même avec les jours
+        //ici, le cas où les jours sont différents
+        if(this.day != other.day){
+            return (this.day < other.day)?-1:1;
+        }
+
+        //tous les cas où les dates ne pouvaient être égales ont été écartés, donc 
+        //date et other sont forcément égales
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object other){
+        if(other instanceof Date){
+            Date d = (Date)other;
+            return this.year == d.year && this.month == d.month && this.day == d.day;
+        }else return false;
+    }
 
 }
--- code/tp3-date/src/test/java/fr/istic/vv/DateTest.java
+++ code/tp3-date/src/test/java/fr/istic/vv/DateTest.java
@@ -1,10 +1,204 @@
 package fr.istic.vv;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.lang.Exception.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import fr.istic.vv.Date;
+
 class DateTest {
+
+    //tests sur le constructeur où une Excepetion est attendue
+    //permet également de commencer à tester isValidDate qui est call dedans
+    @Test 
+    public void testConstruException(){
+        assertThrows(IllegalArgumentException.class, () -> {new Date(-1,12,2001);});
+    }
+
+    @Test 
+    public void testConstruException2(){
+        assertThrows(IllegalArgumentException.class, () -> {new Date(33,12,2001);});
+    }
+
+    @Test 
+    public void testConstruException3(){
+        assertThrows(IllegalArgumentException.class, () -> {new Date(22,13,2001);});
+    }
+
+    @Test 
+    public void testConstruException4(){
+        assertThrows(IllegalArgumentException.class, () -> {new Date(12,12,0);});
+    }
+
+    //tests de isLeapYear
+    @Test 
+    public void testLeapYearNOk(){
+        assertFalse(Date.isLeapYear(1900));
+    }
+
+    @Test
+    public void testLeapYearOk(){
+        //pas divisible par 400, divisible par 4, et pas 100
+        assertTrue(Date.isLeapYear(2024));
+    }
+
+
+    //tests de isValidDate
+    @Test
+    public void testisValideDateOkFevrierB(){
+        //annee bissextile 29 fevrier
+        assertTrue(Date.isValidDate(29, 2, 2024));
+    }
+
+    @Test
+    public void testisValideDateOkFevrierNonB(){
+        //annee bissextile 29 fevrier
+        assertTrue(Date.isValidDate(28, 2, 2025));
+    }
+
+    @Test
+    public void testisValideDateOkMoisDe31J(){
+        //annee bissextile 29 fevrier
+
+        assertTrue(Date.isValidDate(31, 10, 2025));
+    }
+
+    @Test
+    public void testisValideDateOkMoisDe30J(){
+        //annee bissextile 29 fevrier
+        assertTrue(Date.isValidDate(30, 9, 2025));
+    }
+
+    //Tests de previousDate
+    @Test
+    public void testPreviousDateNewYear(){
+        Date newYear = new Date(1, 1, 2001);
+        Date lastPreviousYear = new Date(31,12,2000);
+        assertEquals(newYear.previousDate(), lastPreviousYear);
+    }
+
+    @Test
+    public void testPreviousDateMoisPrecA30J(){
+        Date mois31 = new Date(1, 10, 2001);
+        Date mois30 = new Date(30,9,2001);
+        assertEquals(mois31.previousDate(), mois30);
+    }
+
+    @Test
+    public void testPreviousDateMoisPrecA31J(){
+        Date current = new Date(01, 11, 2001);
+        Date newDate = new Date(31,10,2001);
+        assertEquals(current.previousDate(), newDate);
+    }    
+    
+    @Test
+    public void testPreviousDatePremierMarsB(){
+        Date newYear = new Date(01, 3, 2024);
+        Date lastPreviousYear = new Date(29,2,2024);
+        assertEquals(newYear.previousDate(), lastPreviousYear);
+    }    
+    
+    @Test
+    public void testPreviousDatePremierMarsNonB(){
+        Date newYear = new Date(01, 3, 2003);
+        Date lastPreviousYear = new Date(28,2,2003);
+        assertEquals(newYear.previousDate(), lastPreviousYear);
+    }
+
+    //Tests de nextDate
+    @Test
+    public void testNexDateNewYear(){
+        Date newYear = new Date(1, 1, 2001);
+        Date lastPreviousYear = new Date(31,12,2000);
+        assertEquals(newYear, lastPreviousYear.nextDate());
+    }
+
+    @Test
+    public void testNextDateMoisSuivA30J(){
+        Date mois31 = new Date(1, 10, 2001);
+        Date mois30 = new Date(30,9,2001);
+        assertEquals(mois31, mois30.nextDate());
+    }
+
+    @Test
+    public void testNextDateMoisSuivA31J(){
+        Date current = new Date(01, 11, 2001);
+        Date newDate = new Date(31,10,2001);
+        assertEquals(current, newDate.nextDate());
+    }    
+    
+    @Test
+    public void testNextDatePremierMarsB(){
+        Date premierMars = new Date(01, 3, 2024);
+        Date lastFevrier = new Date(29,2,2024);
+        assertEquals(lastFevrier.nextDate(), premierMars);
+    }    
+    
+    @Test
+    public void testNextDatePremierMarsNonB(){
+        Date premierMars = new Date(01, 3, 2003);
+        Date lastFevrier = new Date(28,2,2003);
+        assertEquals(lastFevrier.nextDate(), premierMars);
+    }
+
+    //Tests de compareTo
+    @Test 
+    public void testCompareToException(){
+        assertThrows(NullPointerException.class, () -> {new Date(22,12,1900).compareTo(null);});
+    }
+
+    @Test
+    public void testCompareToYearDiffSup(){
+        Date current = new Date(01, 3, 2003);
+        Date aTester = new Date(01,3,2004);
+        assertEquals(current.compareTo(aTester), -1);
+    }
+
+    @Test
+    public void testCompareToYearDiffInf(){
+        Date current = new Date(01, 3, 2003);
+        Date aTester = new Date(01,3,2004);
+        assertEquals(aTester.compareTo(current), 1);
+    }
+
+
+    @Test
+    public void testCompareToMonthSup(){
+        Date current = new Date(01, 2, 2004);
+        Date aTester = new Date(01,3,2004);
+        assertEquals(current.compareTo(aTester), -1);
+    }
+
+    @Test
+    public void testCompareToMonthInf(){
+        Date current = new Date(01, 2, 2004);
+        Date aTester = new Date(01,3,2004);
+        assertEquals(aTester.compareTo(current), 1);
+    }
+
+    @Test
+    public void testCompareToDaySup(){
+        Date current = new Date(11, 2, 2004);
+        Date aTester = new Date(12,2,2004);
+        assertEquals(current.compareTo(aTester), -1);
+    }
+
+    @Test
+    public void testCompareToDayInf(){
+        Date current = new Date(11, 2, 2004);
+        Date aTester = new Date(12,2,2004);
+        assertEquals(aTester.compareTo(current), 1);
+    }
+
+    @Test
+    public void testCompareToSameDate(){
+        Date current = new Date(1, 2, 2004);
+        Date aTester = new Date(01,2,2004);
+        assertEquals(aTester.compareTo(current), 0);
+    }
 
 
 }
